6705e5c9acb7e70d28a847c3cb38d6f95c49b36a
jay <hyeonjaehyeog9@gmail.com>
2025-08-29 15:30:55 +0900

    divide forms into 3 phases: nickname, room, message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,94 +1,115 @@
-const socket = io();
+const socket = io()
+// io는 백엔드와 socketIO를 자동적으로 연결해주는 function
+// io function은 알아서 socket.io를 실행하고 있는 서버를 찾음
 
-const welcome = document.getElementById("welcome")
-const form = welcome.querySelector("#welcome #roomName");
-const form2 = welcome.querySelector("#welcome #name")
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 const room = document.getElementById("room");
+const nick = document.getElementById("nickname");
+const nickForm = nick.querySelector("form")
+// const formRoom = room.querySelector("form")
 
+welcome.hidden = true;
 room.hidden = true;
-
 let roomName;
 
 function addMessage(message){
-    console.log(message)
     const ul = room.querySelector("ul");
     const li = document.createElement("li");
-    li.innerText = message
-    ul.appendChild(li)
+    li.innerHTML = message;
+    console.log(message)
+    ul.appendChild(li);
 }
 
-function handleMessageSubmit(event){
-    event.preventDefault()
-    const input = room.querySelector("#msg input");
-    const value = input.value;
-    socket.emit("new_message", input.value, roomName ,()=>{
-        addMessage(`You: ${value}`);
-    });
-    input.value = "";
-}
-function handleNicknameSubmit(event){
-    event.preventDefault();
-    const nick = form2.querySelector("input")
-    // console.log(nick.value)
-    const value = nick.value;
-    socket.emit("nickname", value );
-}
-
-
-function showRoom(){
+function backEnddone(){
     welcome.hidden = true;
     room.hidden = false;
-    const h3 = room.querySelector("h3");
+    const h3 = room.querySelector("h3")
     h3.innerText = `Room ${roomName}`
     const msgForm = room.querySelector("#msg")
-    msgForm.addEventListener("submit", handleMessageSubmit)
+    msgForm.addEventListener("submit", handleMessage)
+
+    // const nameForm = room.querySelector("#name")
+    // nameForm.addEventListener("submit", handleNickName)
+
 }
 
-function handleRoomSubmit(event){
+function nicknameSet(msg){
+    nick.hidden = true;
+    welcome.hidden = false;
+    const h2 = document.querySelector("h2");
+    h2.innerText = `Welcome ${msg}`
+
+}
+
+
+function handleSubmit(event){
     event.preventDefault();
-
-    const room = form.querySelector("input")
-    console.log(room.value)
-
-    const nick = form2.querySelector("input")
-    console.log(nick.value)
-    // console.log()
-
-    socket.emit("enter_room", 
-    room.value, nick.value,
-    showRoom
-    )
-    roomName = room.value;
-    room.value = ""
+    const input = form.querySelector("input")
+    socket.emit("enter_room", input.value, backEnddone)
+    // 1. 특정한 이벤트를 이름에 상관없이 emit해줄 수 있음
+    // 2. object 전송 가능. string만 전송할 필요 x
+    // 3. 서버에서 함수를 호출할 거임. 근데 그 함수는 프론트에 있음
+    // 이게 왜 대단한 걸까?
+    // 보통 웹소켓/네트워크 통신은 단방향 이벤트 기반
+    // -> 근데 socket.io의 emit 마지막 인자에 콜백을 넣을 경우
+    // -> request/response 스타일이 구현 가능함 (양방향 통신 쌉가능)
+    // -> 즉 http 처럼 요청/응답 패턴을 소캣에서 흉내낼 수 있음
+    roomName = input.value;
+    input.value = "";
 }
 
-form2.addEventListener("submit", handleNicknameSubmit)
-form.addEventListener("submit", handleRoomSubmit)
+function handleMessage(event){
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
+    input.value = ""
+}
 
-socket.on("welcome", (user, newCount) =>{
-    const h3 = room.querySelector("h3");
+function handleNickName(event){
+    event.preventDefault();
+    const input = nick.querySelector("#name input");
+    const value = input.value;
+    socket.emit("nickname", value, nicknameSet);
+    input.value = ""
+}
+
+
+
+form.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickName)
+
+socket.on("welcome", (user, newCount)=>{
+    const h3 = room.querySelector("h3")
     h3.innerText = `Room ${roomName} (${newCount})`
-    addMessage(`${user} joined!`);
+    addMessage(`${user} Joined!`)
+});
+
+socket.on("bye", (left, newCount)=>{
+    const h3 = room.querySelector("h3")
+    h3.innerText = `Room ${roomName} (${newCount})`
+    addMessage(`${left} left ㅠㅠ`)
 })
 
-socket.on("bye", (left, newCount) =>{
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName} (${newCount})`
-    addMessage(`${left} left!`);
+socket.on("new_message", msg =>{
+    addMessage(msg);
 })
 
-socket.on("new_message", addMessage)
+socket.on("nickname", nicknameSet)
 
-socket.on("room_change", (rooms)=>{
-    const roomLists = welcome.querySelector("ul");
-    roomLists.innerHTML = "";
+socket.on("room_change", ((rooms) => {
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML = "";
     if(rooms.length === 0){
-        
+        roomList.innerHTML = "";
         return;
     }
-    rooms.forEach((room) =>{
+    rooms.forEach(room => {
         const li = document.createElement("li");
-        li.innerText = room;
-        roomLists.append(li);
-    });
-})
+        li.innerHTML = room;
+        roomList.append(li);
+    })
+}))
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from "http";
 import express from "express";
-import SocketIO from "socket.io";
-import { count } from "console";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 const port = 3000;
@@ -14,50 +14,67 @@ app.get("/", (req, res) => res.render("home"));
 const handleListen = () => console.log(`listening on port: ${port}`)
 
 const httpserver = http.createServer(app);
-const wsServer = SocketIO(httpserver);
+const wsServer = new Server(httpserver,{
+    cors: {
+        origin: ["https://admin.socket.io"],
+        Credential: true,
+    },
+});
+instrument(wsServer, {
+    auth:false
+});
 
-function publicRooms(){
+function publicRoooms(){
     const sids = wsServer.sockets.adapter.sids;
     const rooms = wsServer.sockets.adapter.rooms;
+
     const publicRooms = [];
-    rooms.forEach((_, key) => {
+    rooms.forEach((_,key) => {
         if(sids.get(key) === undefined){
-            publicRooms.push(key);
-        }
+            publicRooms.push(key)
+        }     
     });
     return publicRooms;
 }
 
 function countRoom(roomName){
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size
 }
 
-wsServer.on("connection", socket => {
-    socket["nickname"] = "Anon"
-
-    console.log(" wtwttffff ",socket.nickname)
-    socket.onAny((event)=>{
-        console.log(`Socket Event:${event}`)
+wsServer.on("connection", socket=>{
+    socket.nickname = "Anon";
+// connection은 socket.io에서 정한 예약 이벤트 이름
+// connection은 클라이언트 연결이 발생할 때만 실행되는 고정 이벤트
+    socket.onAny((event) =>{
+        console.log(wsServer.sockets.adapter);
+        console.log(`Socket Event: ${event}`)
     })
-    socket.on("enter_room", (roomName,nick, done) =>{
-        socket.nickname = nick
-        socket.join(roomName);
-        done();
-        socket.to(roomName).emit("welcome" , nick, countRoom(roomName));
-        wsServer.sockets.emit("room_change", publicRooms());
+    socket.on("enter_room", (roomName, done) =>{
+    // enter_room 은 커스텀 이벤트라 사용자 마음대로 설정 가능
+        socket.join(roomName); // roomName 이름인 방에 join
+        done()
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+        wsServer.sockets.emit("room_change", publicRoooms());
     })
-    socket.on("disconnecting", () => { // 여기 어떻게 잘 해야할듯
-        socket.rooms.forEach((room) =>
-            socket.to(room).emit("bye", socket.nickname, countRoom(room) -1));
-        
-    });
+    socket.on("disconnecting", () => {
+        // console.log("when disc ", socket.rooms)
+        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname, countRoom(room) -1));
+        // socket.rooms는 각각의 소켓 안에 있는 set인거임
+        // Ex) Set(1) { '2fTY8yKuTbxqDGAwAAAB' }
+    })
     socket.on("disconnect", ()=>{
-        wsServer.sockets.emit("room_change", publicRooms());
+        wsServer.sockets.emit("room_change", publicRoooms());
     })
     socket.on("new_message", (msg, room, done)=>{
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`)
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
     })
-    socket.on("nickname", (nickname) => socket["nickname"] = nickname)
+    socket.on("nickname", (nickname, done) => {
+        socket["nickname"] = nickname;
+        done();
+        socket.emit("nickname", socket.nickname)
+    })
+    // socket.on()
 })
+
 httpserver.listen(port, handleListen)
